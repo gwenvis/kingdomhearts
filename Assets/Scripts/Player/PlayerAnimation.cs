5677a64650e8069280c17a7e18cca1b71b3d5de3
Timo <timo.heijne@kpnplanet.nl>
2017-12-04 09:38:56 +0100

    Further Development on attacking

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -8,18 +8,20 @@ public class PlayerAnimation : MonoBehaviour {
 
     public enum Status {
         Idle,
-        Walking,
-        Attacking
+        Walking
     }
 
     public enum AttackType {
-        One,
-        Two,
-        Three,
-        Fail
+        None = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Fail = 4       
     }
 
     private Status playerStatus = Status.Idle;
+    private AttackType attackType = AttackType.None;
+    private AttackType attackQueue = AttackType.None;
 
     [SerializeField]
     private Animator animController;
@@ -35,8 +37,21 @@ public class PlayerAnimation : MonoBehaviour {
     /// </summary>
     /// <param name="type"></param>
     public void PlayAttackNumber(AttackType type) {
-        animController.SetInteger("Attack Type", (int)type);
-        animController.SetTrigger("Attack");
+        Debug.Log(type + " " + attackQueue);
+
+        if (GetBool("Attack")) {
+            // Animation is playing so we set Queue
+            if(attackQueue == AttackType.None) // If attackQueue is set we ignore this new input.
+                attackQueue = type;
+        }
+        else {
+            // No animation is playing so we can play one
+            animController.SetInteger("Attack Type", (int)type);
+            animController.SetTrigger("Attack");
+            attackType = type;
+
+            StartCoroutine(WaitForAnimToBeDone());
+        }     
     }
 
     /// <summary>
@@ -58,4 +73,24 @@ public class PlayerAnimation : MonoBehaviour {
             animController.SetBool("Walking", false);
         }
     }
+
+    public AttackType GetAttackType() {
+        return attackType;
+    }
+
+    public bool GetBool(string name) {
+        return animController.GetBool(name);
+    }
+
+    IEnumerator WaitForAnimToBeDone() {
+        yield return new WaitUntil(() => GetBool("Attack") == false);
+        yield return new WaitForSeconds(3f);
+        if (attackQueue != AttackType.None) {
+            PlayAttackNumber(attackQueue);
+            attackQueue = AttackType.None;
+        } else {
+            attackType = AttackType.None;
+        }
+    }
 }
+
--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿// Created by Timo Heijne
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,15 +8,40 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour {
 
     [SerializeField] private PlayerAnimation _playerAnimation;
+    [SerializeField] private Transform _enemy;
 
-	// Use this for initialization
-	void Start () {
-		if(_playerAnimation == null)
+    // Use this for initialization
+    void Start() {
+        if (_playerAnimation == null)
             Debug.LogError("PlayerAttack :: Player Animation Is Null");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+
+            float distance = Vector3.Distance(transform.position, _enemy.position);
+            float dot = Vector3.Dot(Vector3.forward, transform.InverseTransformPoint(_enemy.position));
+
+            if (dot > 0.4 && distance < 5) {
+                switch (_playerAnimation.GetAttackType()) {
+                    case PlayerAnimation.AttackType.None:
+                        _playerAnimation.PlayAttackNumber(PlayerAnimation.AttackType.One);
+                        break;
+                    case PlayerAnimation.AttackType.One:
+                        _playerAnimation.PlayAttackNumber(PlayerAnimation.AttackType.Two);
+                        break;
+                    case PlayerAnimation.AttackType.Two:
+                        _playerAnimation.PlayAttackNumber(PlayerAnimation.AttackType.Three);
+                        break;
+                    case PlayerAnimation.AttackType.Three:
+                        _playerAnimation.PlayAttackNumber(PlayerAnimation.AttackType.One);
+                        break;
+                }
+            } else {
+                //_playerAnimation.PlayAttackNumber(PlayerAnimation.AttackType.Fail);
+                Debug.LogWarning("Test");
+            }
+        }
+    }    
 }
