0fdfa7ecd2f55d7aa0e721f48bae723d11e8f5b5
Antonio Bottelier <Antonio3338@gmail.com>
2017-12-10 21:14:57 +0100

    I forgot to add the scene and script

---
+++ Assets/Scripts/AI/AIBall.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AIBall : MonoBehaviour
+{
+    private float speed = 15;
+    private float height = 2;
+    private float x, a, c;
+    private float distance;
+
+    private Vector3 origin, direction;
+
+    public void SetValues(Vector3 origin, Vector3 direction, float distance)
+    {
+        this.distance = distance;
+        this.origin = origin;
+        this.direction = direction;
+        this.direction.y = 0;
+        this.direction.Normalize();
+        x = 0;
+
+        c = distance / 2;
+        a = height / (c * c);
+    }
+
+    private void Update()
+    {
+        // y = -(a(x-c)^2) + b
+        float y = -(a*((x-c)*(x-c))) + height;
+
+        Debug.Log(y);
+
+        var pos = direction * x;
+        pos.y = y;
+        transform.position = origin + pos;
+        
+        x += Time.deltaTime * speed;
+
+    }
+}
