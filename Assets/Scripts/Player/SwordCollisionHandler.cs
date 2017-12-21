41cb9187560794162e2f5bea85d6a588aa756e0f
Timo <timo.heijne@kpnplanet.nl>
2017-12-21 08:37:06 +0100

    Replaced a few world files, added hit 'Target'

---
+++ Assets/Scripts/Player/SwordCollisionHandler.cs
@@ -0,0 +1,27 @@
+﻿// Created by Timo Heijne
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordCollisionHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		throw new NotImplementedException();
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		Target.instance.ShowAttack();
+	}
+}
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -17,15 +17,37 @@ public class Target : MonoBehaviour {
 
     [SerializeField] private Image _image;
 
-	private void Update () {
+	private bool _isAttacking = false;
+
+	public static Target instance;
+
+	private void Start() {
+		instance = this;
+	}
+
+	private void Update () {	
 		float dist = Vector3.Distance(_target.transform.position, _player.transform.position);
-		
-	    _image.sprite = dist < 3f ? _attackRange : _idle;
+
+		if (_isAttacking) {
+			_image.sprite = _hit;
+		} else {
+			_image.sprite = dist < 3f ? _attackRange : _idle;
+		}			
 		
 		float size = Math.Min(450/dist, 70);
 		_image.rectTransform.sizeDelta = new Vector2(size,size);
 		
-	    Vector3 pos = Camera.main.WorldToScreenPoint(_target.transform.position);
-        transform.position = pos;
+		Vector3 pos = Camera.main.WorldToScreenPoint(_target.transform.position);
+		transform.position = pos;	
+	}
+
+	public void ShowAttack() {
+		StartCoroutine(AttackIcon());
+	}
+
+	IEnumerator AttackIcon() {
+		_isAttacking = true;
+		yield return new WaitForSecondsRealtime(1f);
+		_isAttacking = false;
 	}
 }
